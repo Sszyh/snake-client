a5e5d14acdb99b57bd573c23df404f36910eafee
Sszyh <zhuyer@outlook.com>
2022-07-06 13:56:27 -0700

    add client

---
+++ client.js
@@ -0,0 +1,32 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243",
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("connected")
+    // code that does something when the connection is first established
+  });
+
+  conn.on("data", (data) => {
+    console.log("server says", data);
+    // code that does something when the connection is first established
+  });
+
+  return conn;
+
+  
+};
+
+
+//console.log("Connecting ...");
+//connect();
+
+module.exports = connect;
