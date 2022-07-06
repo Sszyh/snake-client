b02f6ece305abfec29d8738606de62394c9d4c9d
Sszyh <zhuyer@outlook.com>
2022-07-06 14:53:21 -0700

    identify control + c

--- play.js
+++ play.js
@@ -31,6 +31,31 @@ const connect = require("./client.js");
 console.log("Connecting ...");
 connect();
 
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+    process.exit();
+   }
+  });
+  
+
+  
+
+  return stdin;
+};
+
+const handleUserInput = function () {
+
+};
 
 
 
+setupInput();
