ebdd0f3ed8ecc5448e12b8099124b9ef18647f44
Sszyh <zhuyer@outlook.com>
2022-07-06 13:57:37 -0700

    modified play

--- play.js
+++ play.js
@@ -1,31 +1,36 @@
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("connect", () => {
-    console.log("connected")
-    // code that does something when the connection is first established
-  });
-
-  conn.on("data", (data) => {
-    console.log("server says", data);
-    // code that does something when the connection is first established
-  });
-
-  return conn;
-
-  
-};
-
+//const connect = function () {
+//  const conn = net.createConnection({
+//    host: "165.227.47.243",
+//    port: 50541
+//  });
+//
+//  // interpret incoming data as text
+//  conn.setEncoding("utf8");
+//
+//  conn.on("connect", () => {
+//    console.log("connected")
+//    // code that does something when the connection is first established
+//  });
+//
+//  conn.on("data", (data) => {
+//    console.log("server says", data);
+//    // code that does something when the connection is first established
+//  });
+//
+//  return conn;
+//
+//  
+//};
+//
+const connect = require("./client.js");
 
 console.log("Connecting ...");
 connect();
+
+
+
 
