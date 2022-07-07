6685ca4523821b225595df75b150d3c6a7fbdfd0
Sszyh <zhuyer@outlook.com>
2022-07-06 20:40:05 -0700

    add constants

---
+++ constants.js
@@ -0,0 +1,16 @@
+
+
+//const serverIp = {
+//  IP: "165.227.47.243",
+//  PORT: 50541
+//};
+//
+//module.exports = serverIp;
+
+const IP = "165.227.47.243";
+const PORT = 50541;
+
+module.exports = {
+  IP, PORT
+}
+
